c889e4fa8f35667f5efbbf770d6c01ba8a3f2646
MrErenK <akbaseren4751@gmail.com>
2025-08-10 15:46:07 +0300

    fix: Unneeded icon definitions in metadata

--- src/config/metadata.ts
+++ src/config/metadata.ts
@@ -45,30 +45,6 @@ export function generateMetadata(config?: SiteConfig): Metadata {
         "max-snippet": -1,
       },
     },
-    icons: {
-      icon: "/favicon.ico",
-      shortcut: "/favicon-16x16.png",
-      apple: "/apple-touch-icon.png",
-      other: [
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "32x32",
-          url: "/favicon-32x32.png",
-        },
-        {
-          rel: "icon",
-          type: "image/png",
-          sizes: "16x16",
-          url: "/favicon-16x16.png",
-        },
-        {
-          rel: "manifest",
-          url: "/site.webmanifest",
-        },
-      ],
-    },
-    manifest: "/site.webmanifest",
   };
 }
 
